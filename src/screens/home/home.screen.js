b8031465138afc46b8e41ef6fd88ff2947286287
Ederson Machado <edersonmberti@gmail.com>
2020-08-18 13:57:28 -0300

    add basic home screen layout

--- src/screens/home/home.screen.js
+++ src/screens/home/home.screen.js
@@ -1,59 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { PlaylistCard } from '../../components'
+
+import Playlists from '../../mock/data';
+
+import Styles from './home.style';
 
 export const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Playlists</Text>
-      <View style={styles.cardContainer}>
-        <Image
-          source={{
-            uri: 'https://i.ytimg.com/vi/4j1v3lZDvE4/maxresdefault.jpg',
-          }}
-          style={styles.image}
-        />
-        <View style={styles.textWrapper}>
-          <Text style={styles.cardTitle}>Chill</Text>
-          <Text style={styles.cardDescription}>8 videos . 18:30 min</Text>
-        </View>
-      </View>
+    <View style={Styles.container}>
+      <Text style={Styles.title}>Playlists</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {Playlists.map((props) => (
+          <PlaylistCard {...props} key={props.id} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 30,
-    marginHorizontal: 15,
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 38,
-    marginBottom: 20,
-    color: '#414141',
-  },
-  cardContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  image: {
-    width: 150,
-    height: 100,
-    borderRadius: 10,
-  },
-  textWrapper: {
-    marginLeft: 10,
-  },
-  cardTitle: {
-    fontSize: 20,
-    lineHeight: 24,
-    color: '#414141',
-  },
-  cardDescription: {
-    fontSize: 12,
-    lineHeight: 14,
-    color: '#A0A0A0',
-  },
-});
---
+++ src/screens/home/home.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Platform } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 30,
+    paddingHorizontal: 15,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    lineHeight: 38,
+    marginBottom: 10,
+    color: '#414141',
+  },
+});
+
+export default styles;
